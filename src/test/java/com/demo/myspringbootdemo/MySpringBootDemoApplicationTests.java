ec1c69275905b9bedec6768a232f1b9e8fbbc344
Ziv Chan <zivchan.tw@gmail.com>
2022-07-22 13:05:54 +0800

    first commit

---
+++ src/test/java/com/demo/myspringbootdemo/MySpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.myspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
